951beac9839b61f9cd427f22982e002fa40f55ae
Anoke <rustamgta1011@gmail.com>
2024-03-08 22:17:59 +0300

    3005

---
+++ Easy/3005_CountElementsWithMaxFrequency.go
@@ -0,0 +1,22 @@
+func maxFrequencyElements(nums []int) int {
+	dictionary := make(map[int]int)
+
+	for _, num := range nums {
+		dictionary[num]++
+	}
+
+	total := 0
+	maxValue := 0
+
+	for _, i2 := range dictionary {
+		if i2 == maxValue {
+			total += i2
+		}
+		if i2 > maxValue {
+			maxValue = i2
+			total = i2
+		}
+	}
+
+	return total
+}
