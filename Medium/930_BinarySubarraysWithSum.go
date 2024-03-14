990384dbf0ccd70ef334c058f1dce3582c0db238
Anoke <rustamgta1011@gmail.com>
2024-03-14 23:09:15 +0300

    930

---
+++ Medium/930_BinarySubarraysWithSum.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func numSubarraysWithSum(nums []int, goal int) int {
+	totalCount := 0
+	currentSum := 0
+	freq := make(map[int]int)
+	for _, num := range nums {
+		currentSum += num
+		if currentSum == goal {
+			totalCount++
+		}
+		if _, ok := freq[currentSum-goal]; ok {
+			totalCount += freq[currentSum-goal]
+		}
+		freq[currentSum]++
+	}
+	return totalCount
+}
+
+func main() {
+	nums := []int{1, 0, 1, 0, 1}
+	fmt.Println(numSubarraysWithSum(nums, 2))
+}
